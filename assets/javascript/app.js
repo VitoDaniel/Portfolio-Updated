c95be9e7a9f36f95b9c0b0e35545c34fa1366c50
Viktor Daniyelyan <35073289+VitoDaniel@users.noreply.github.com>
2018-08-12 12:58:17 -0400

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    $(".dropdown-trigger").dropdown();
+
+    $(".sidenav").sidenav();
+
+    $('.parallax').parallax();
+  
+    $('.carousel').carousel();
+
+});
